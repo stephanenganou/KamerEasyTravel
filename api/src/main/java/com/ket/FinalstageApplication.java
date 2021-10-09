f4ac3c7cce2916f82c24da6221b851a80dd80bf0
Nganou Wafo, Stephane <stephane.nganou.wafo@ausy-technologies.de>
2021-10-09 03:09:51 +0200

    adds api project

---
+++ api/src/main/java/com/ket/FinalstageApplication.java
@@ -0,0 +1,13 @@
+package com.ket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalstageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalstageApplication.class, args);
+    }
+
+}
